14b4102f7d1e6ba463fb9f97f30a165ada2b8072
Christopher Marzulli <chriszulli15@gmail.com>
2024-04-15 11:43:06 -0400

    added faux info to footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -7,13 +7,20 @@ function Footer(){
     <div id="footer">
       <div className="footer-sec1">
         <img src={Logo} />
-        <p>"Who's Burger" is not a real resturant.<br/>It is a personal project.</p>
+        <p className="footer-p1">"Who's Burger" is not a real resturant.<br/>It is a personal project.</p>
       </div>
-      <div>
-
+      <div className="footer-sec2">
+        <p>Locations:</p>
+        <p>Address, PA</p>
+        <p>Address, NY</p>
+        <p>Address, NJ</p>
       </div>
-      <div>
-        
+      <div className="footer-sec3">
+        <p>Contacts:</p>
+        <p>PA, (555)555-5000</p>
+        <p>NY, (555)555-5000</p>
+        <p>NJ, (555)555-5000</p>
+        <p>You can also find us on <br/>Doordash and UberEats</p>
       </div>
     </div>
   )
